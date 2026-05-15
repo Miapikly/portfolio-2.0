67b9ef0b7615c0d0f482de823da23152a82ea091
Jessica Quezada <jqueza755@west-mec.org>
2026-05-14 17:07:44 -0700

    post

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return (
+        < section id="home" className="min-h-screen flex items-center justify-center relative" >
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 gradient-text leading-right">Hi, I'm Miapikly</h1>
+                <p>
+                    bruger buegre burger guebrube biurger buerger burger burger burger burger bubrger bugre burger buger burger
+                </p>
+            </div>
+        </section>
+    );
+}
